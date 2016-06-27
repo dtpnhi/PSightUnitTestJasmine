19b411ebb65eef792dafa507332d751f0986d732
Phuongnhi Dang <dtpnhi@gmail.com>
2016-06-27 13:19:21 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var gulpPlugins = require('gulp-load-plugins');
+
+var argv = require('yargs').argv;
+var wiredep = require('wiredep')();
+
+gulp.task('inject-js', function () {
+    
+    return gulp
+        .src()
+        ;
+    
+});
